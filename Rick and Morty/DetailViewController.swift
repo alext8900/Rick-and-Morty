7d47fd976553a7a950a99b1467f908be624376bc
Christian Lorenzo <clbmiami2004@gmail.com>
2020-02-20 20:59:18 -0800

    Created a DetailViewController document in order to control all the details from the ResultsViewController/ Also, created a ResultsViewController which is going to be in charged of showing all the results from the API call by showing an image and the image name.

---
+++ Rick and Morty/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Rick and Morty
+//
+//  Created by Lambda_School_Loaner_201 on 2/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
