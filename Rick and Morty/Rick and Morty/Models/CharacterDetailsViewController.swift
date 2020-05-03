bbf6d9c25bdc84811ddddd6fa6a4a10fdc868cc8
Alex Thompson <guitarman226@gmail.com>
2020-05-03 04:56:06 -0500

    Completed until further notice

--- Rick and Morty/Rick and Morty/Models/CharacterDetailsViewController.swift
+++ Rick and Morty/Rick and Morty/Models/CharacterDetailsViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import SDWebImage
 
 class CharacterDetailsViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private let viewModel: CharactersDetailViewModel
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(viewModel: CharactersDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: "CharacterDetailsViewController", bundle: Bundle.main)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("Some stuff happened")
+    }
+    
+    func updateUI() {
+        nameLabel.text = viewModel.character.name
+        statusLabel.text = viewModel.character.status
+        speciesLabel.text = viewModel.character.species
+        genderLabel.text = viewModel.character.gender
+        originLabel.text = viewModel.character.origin
+        locationLabel.text = viewModel.character.location
+        imageView.sd_setImage(with: viewModel.character.imageUrl, placeholderImage: UIImage(named: "placeholder"))
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
 }
